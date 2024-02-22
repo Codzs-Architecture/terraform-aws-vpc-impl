f0470f42cf4053809f6572a029679d03db69a10b
Nitin Khaitan <khaitan.nitin@gmail.com>
2024-02-22 16:35:56 +1100

    clenaup

--- variables.tf
+++ variables.tf
@@ -2,8 +2,3 @@ variable "account_customizations_name" {
   type    = string
   description = "Account identifier as provided for the account in aft-request"
 }
-
-variable "region" {
-  type    = string
-  default = "ap-southeast-2"
-}
